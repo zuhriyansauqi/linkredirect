eb8a380759d930fee42b7e43c52c8770a72a0a7a
Muhammad Zuhriyan Sauqi <hello@zuhriyansauqi.com>
2021-06-08 09:10:57 +0700

    first commit

---
+++ src/components/LinkRedirect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="link-redirect">
+    {{ loadingText }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+let loadingText = ref('Please wait ...')
+
+setTimeout(() => {
+  const { link } = route.query
+  if (link) {
+    window.location.href = link
+  }
+}, 3000)
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <link-redirect />
+  </div>
+</template>
+
+<script setup>
+import LinkRedirect from '../components/LinkRedirect.vue'
+</script>
